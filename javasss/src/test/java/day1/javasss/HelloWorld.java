5d460eebf28030bca9e998b6cfd0d0cf517b7245
Ramamohan Reddy Siddareddy <Ramamohan.Siddareddy@cdk.com>
2017-12-10 12:59:24 +0530

    First commit

---
+++ javasss/src/test/java/day1/javasss/HelloWorld.java
@@ -0,0 +1,10 @@
+package day1.javasss;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
